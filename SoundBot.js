44bdd582a8bb6e6fd376db795d1ea9962952d7b0
RileyMitchum <qboyrbm@aol.com>
2016-11-19 10:26:25 -0600

    Update SoundBot.js
    Commented out non-working audio, and added version numbers.

--- SoundBot.js
+++ SoundBot.js
@@ -2,33 +2,60 @@ const config = require('./config/default.json');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var version = '1.1';
 client.login(config.token); 
 
 client.on("message", msg => 
 {
-    	if (msg.content.startsWith("!allahu akbar")) 
+    if (msg.content.startsWith("!Allahu Akbar")) 
 	{
-		const voiceChannel = msg.member.voiceChannel;
 		const file = `sound/akbar1.mp3`;
-		
-		if (voiceChannel != undefined)
-		{
-			playSound(file, voiceChannel, msg);
-		}
+		playSound(file, msg);
 	}
 	
 	if (msg.content.startsWith("!TerroristsWin")) 
 	{
-		const voiceChannel = msg.member.voiceChannel;
 		const file = `sound/csgoTerroristsWin.mp3`;
-		
-		if (voiceChannel != undefined)
-		{
-			playSound(file, voiceChannel, msg);
-		}
+		playSound(file, msg);
 	}
 	
-	if (msg.content === '!RileysCommands') {
+	if (msg.content.startsWith("!Genius")) 
+	{
+		const file = `sound/genius.mp3`;
+		playSound(file, msg);
+	}
+	
+/* 	if (msg.content.startsWith("!WhoRYou")) 
+	{
+		const file = `sound/whoRYou.mp3`; // Does not work
+		playSound(file, msg);
+	} */
+	
+/* 	if (msg.content.startsWith("!ProveIt")) 
+	{
+		const file = `sound/proveIt.mp3`; // Does not work
+		playSound(file, msg);
+	} */
+	
+	if (msg.content.startsWith("!UmmmNo")) 
+	{
+		const file = `sound/ummmNo.mp3`;
+		playSound(file, msg);
+	}
+	
+	if (msg.content.startsWith("!NotToday")) 
+	{
+		const file = `sound/notToday.mp3`;
+		playSound(file, msg);
+	}
+	
+/* 	if (msg.content.startsWith("!Now")) 
+	{
+		const file = `sound/now.mp3`; // Does not work
+		playSound(file, msg);
+	} */
+	
+	if (msg.content === '!HelpRiley') {
 		listCommands(msg);
 		return;
 	}
@@ -36,31 +63,43 @@ client.on("message", msg =>
 
 client.on('ready', () => 
 {
-	console.log('Rileys Sound Bot is Running!');
+	console.log('Rileys Sound v' + version + ' Bot is Running!');
 });
 
-function playSound(file, voiceChannel, msg) 
+function playSound(file, msg) 
 {
-	voiceChannel.join().then((connection) => 
+	const voiceChannel = msg.member.voiceChannel;
+	if (voiceChannel != undefined)
 	{
-		const dispatcher = connection.playFile(file);
-		dispatcher.on('end', () => 
+		voiceChannel.join().then((connection) => 
 		{
-			connection.disconnect();
+			const dispatcher = connection.playFile(file);
+			dispatcher.on('end', () => 
+			{
+				connection.disconnect();
+			});
+		}).catch((error) => 
+		{
+			console.log('Error occured!');
+			console.log(error);
 		});
-	}).catch((error) => 
-	{
-		console.log('Error occured!');
-		console.log(error);
-	});
+	}
 }
 
 function listCommands(msg) {
 	const message = [
 	'```',
-	'!RileysCommands  Show this message',
-	'!allahu akbar       Play "ALLAHU AKBAR!" sound',
-	'!TerroristsWin      Play CS GO "Terrorists Win" sound',
+	'Riley\'s Sound Bot v' + version,
+	' ',
+	'!HelpRiley      --- Show this message',
+	'!Allahu Akbar   --- Play "ALLAHU AKBAR!" sound',
+	'!TerroristsWin  --- Play CS GO "Terrorists Win" sound',
+	'!Genius         --- Play "Genius!" sound',
+	'!WhoRYou        --- Does not currently work -- Play "Who are you?" sound',
+	'!ProveIt        --- Does not currently work -- Play "Prove It!" sound',
+	'!UmmmNo         --- Play "Ummmm no?!?!?" sound',
+	'!NotToday       --- Play "Not today!" sound',
+	'!Now            --- Does not currently work -- Play "Now!" sound',
 	'```'
 	];
 	msg.channel.sendMessage(message);
